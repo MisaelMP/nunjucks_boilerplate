40515d209e7f2b31ae282cda25dfd4d212963397
Misael  M <phatarino@gmail.com>
2019-04-25 00:02:19 +1000

    added architecture

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,99 +1,106 @@
 'use strict';
 
-
 // -----------------------------------------------------------------------------
 // Dependencies
 // -----------------------------------------------------------------------------
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sassdoc = require('sassdoc');
-var browserSync = require('browser-sync').create();
-var nunjucksRender = require('gulp-nunjucks-render');
-var concat      = require('gulp-concat');
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var siteOutput = './dist';
-
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sassdoc = require('sassdoc');
+const browserSync = require('browser-sync').create();
+const nunjucksRender = require('gulp-nunjucks-render');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+const siteOutput = './dist';
 
 // -----------------------------------------------------------------------------
 // Configuration
 // -----------------------------------------------------------------------------
 
-var input = './scss/*.scss';
-var inputMain = './scss/main.scss';
-var output = siteOutput + '/css';
-var inputTemplates = './pages/*.html';
-var sassOptions = { outputStyle: 'expanded' };
-var autoprefixerOptions = { browsers: ['last 2 versions', '> 5%', 'Firefox ESR'] };
-var sassdocOptions = { dest: siteOutput + '/sassdoc' };
-
+const input = './scss/*.scss';
+const inputMain = './scss/main.scss';
+const output = siteOutput + '/css';
+const inputTemplates = './pages/*.njk';
+const sassOptions = {
+	outputStyle: 'expanded'
+};
+const autoprefixerOptions = {
+	browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+const sassdocOptions = {
+	dest: siteOutput + '/sassdoc'
+};
 
 // -----------------------------------------------------------------------------
-// Sass compilation
+// Scss compilation
 // -----------------------------------------------------------------------------
 
-gulp.task('sass', function() {
-  return gulp
-    .src(inputMain)
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest(output))
-    .pipe(browserSync.stream());
-});
-
+function scss() {
+	return gulp
+		.src(inputMain)
+		.pipe(sass(sassOptions).on('error', sass.logError))
+		.pipe(autoprefixer(autoprefixerOptions))
+		.pipe(gulp.dest(output))
+		.pipe(browserSync.stream());
+};
 
 // -----------------------------------------------------------------------------
 // Javascript
 // -----------------------------------------------------------------------------
 
-gulp.task('scripts', function() {
-  return gulp.src([
-	  	'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-	  	'js/main.js'
-  	])
-    .pipe(concat({ path: 'main.js'}))
-    .pipe(browserSync.reload({stream:true}))
-    .pipe(gulp.dest(siteOutput + '/js'));
-});
-
+function scripts() {
+	return gulp.src([
+			'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+			'js/main.js'
+		], {
+			allowEmpty: true
+		})
+		.pipe(concat({
+			path: 'main.js'
+		}))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+		.pipe(gulp.dest(siteOutput + '/js'));
+};
 
 // -----------------------------------------------------------------------------
 // Templating
 // -----------------------------------------------------------------------------
 
-gulp.task('nunjucks', function() {
-  nunjucksRender.nunjucks.configure(['./templates/']);
-  // Gets .html and .nunjucks files in pages
-  return gulp.src(inputTemplates)
-  // Renders template with nunjucks
-  .pipe(nunjucksRender())
-  // output files in dist folder
-  .pipe(gulp.dest(siteOutput))
-});
-
+function nunjucks() {
+	nunjucksRender.nunjucks.configure(['./templates/']);
+	// Gets .html and .nunjucks files in pages
+	return gulp.src(inputTemplates)
+		// Renders template with nunjucks
+		.pipe(nunjucksRender())
+		// output files in dist folder
+		.pipe(gulp.dest(siteOutput))
+};
 
 // -----------------------------------------------------------------------------
 // Imagemin
 // -----------------------------------------------------------------------------
 
-gulp.task('img', function() {
-  return gulp.src('./img/**/*')
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{removeViewBox: false}],
-      use: [pngquant()]
-    }))
-    .pipe(gulp.dest(siteOutput + '/img'));
-});
-
+function img() {
+	return gulp.src('./img/**/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{
+				removeViewBox: false
+			}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest(siteOutput + '/img'));
+};
 
 // -----------------------------------------------------------------------------
 // Fonts
 // -----------------------------------------------------------------------------
 
-// gulp.task('fonts', function() {
+// ('fonts', function() {
 //   return gulp.src(['./fonts/*'])
 //   .pipe(gulp.dest(siteOutput + '/fonts/'));
 // });
@@ -103,48 +110,57 @@ gulp.task('img', function() {
 // Sass documentation generation
 // -----------------------------------------------------------------------------
 
-gulp.task('sassdoc', function() {
-  return gulp
-    .src(input)
-    .pipe(sassdoc(sassdocOptions))
-    .resume();
-});
-
+function scssdoc() {
+	return gulp
+		.src(input)
+		.pipe(sassdoc(sassdocOptions))
+		.resume();
+};
 
 // -----------------------------------------------------------------------------
 // Watchers
 // -----------------------------------------------------------------------------
 
-gulp.task('watch', function() {
-    // Watch the sass input folder for change,
-    // and run `sass` task when something happens
-    gulp.watch(input, ['sass']).on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
+function watch() {
+	// Watch the sass input folder for change,
+	// and run `sass` task when something happens
+	gulp.watch(input, gulp.series(scss)).on('change', function(event) {
+		console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+	});
 
-    gulp.watch('./js/*', ['scripts']).on('change', browserSync.reload);
+	gulp.watch('./js/*', gulp.series(scripts)).on('change', browserSync.reload);
 
-    // Watch nunjuck templates and reload browser if change
-    gulp.watch(inputTemplates, ['nunjucks']).on('change', browserSync.reload);
+	//Watch nunjuck templates and reload browser if change
+	gulp.watch(inputTemplates, gulp.series(nunjucks)).on('change', browserSync.reload);
 
-});
-
+};
 
 // -----------------------------------------------------------------------------
 // Static server
 // -----------------------------------------------------------------------------
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-    server: {
-      baseDir: siteOutput
-    }
-  });
-});
+function browserReload() {
+	browserSync.init({
+		server: {
+			baseDir: siteOutput
+		}
+	});
+};
 
+// -----------------------------------------------------------------------------
+// Register tasks
+// -----------------------------------------------------------------------------
+
+exports.watch = watch;
+exports.broserReload = browserReload;
+exports.scss = scss;
+exports.scssdoc = scssdoc;
+exports.scripts = scripts;
+exports.nunjucks = nunjucks;
+exports.img = img;
 
 // -----------------------------------------------------------------------------
 // Default task
 // -----------------------------------------------------------------------------
 
-gulp.task('default', ['sass', 'nunjucks', 'img', 'scripts', 'watch', 'browser-sync']);
+gulp.task('default', gulp.parallel(scss, nunjucks, img, scripts, watch, browserReload));
--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 // main.js
+// import "bulma/css/bulma.css";
 
-$(document).ready(function (){
-	console.log('document is ready bitch');
+$(document).ready(function() {
+
+	// Check for click events on the navbar burger icon
+	$(".navbar-burger").click(function() {
+
+			// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+			$(".navbar-burger").toggleClass("is-active");
+			$(".navbar-menu").toggleClass("is-active");
+	});
 });
